f4cb4b0fbb28c823dfca68d23b549b45f495db7e
haniyehtajer <haniyehtajer@gmail.com>
2025-02-25 11:45:29 -0500

    3,4,5

---
+++ integrator_comparison/MERCURIUS/merc_4/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH --time=330:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=cham_com_4
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=tajer.1@osu.edu
+#SBATCH --partition=ses
+
+
+
+# COMMANDS TO RUN
+
+cd /home/tajer.1/rebound/examples/cham_com_4
+make
+wait
+./rebound
+wait
